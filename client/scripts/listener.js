17d2107c3c4e29ad5ee3bc540829e39d26e36acf
Bård Hovde <bard@cddnation.com>
2015-02-22 16:13:14 +0000

    Release 0.1

--- client/scripts/listener.js
+++ client/scripts/listener.js
@@ -7,6 +7,8 @@ var analogueIO = analogueIO || {};
 
     analogueIO.Listener = function() {
 
+        var self = this;
+
         this.listen = function(targetEl){
 
             console.log('Listening for websocket updates!');
@@ -17,12 +19,38 @@ var analogueIO = analogueIO || {};
             socket.on('update', function(data) {
                 console.log('Socket update received!');
                 target.text(data.value);
+                self.rotate(data.value)
             });
 
             socket.on('error', function() { console.error(arguments) });
             socket.on('message', function() { console.log(arguments) });
 
         };
+
+        /**
+        * Rotate target element
+        */
+
+        this.rotate = function(input) {
+
+            var min = -30;
+            var max = 30;
+            var rotation = self.transpose(input, 100, 900, min, max);
+
+            $('#joystick').css('transform', 'rotate(' + rotation + 'deg)');
+
+        };
+
+        /**
+        * Shared Function to transpose value into a new range of numbers, keeping the ratio from the old range.
+        */
+
+        this.transpose = function(input, oldMin, oldMax, newMin, newMax){
+
+          var oldRange = (oldMax - oldMin);
+          return((((input - oldMin) * (newMax - newMin)) / oldRange) + newMin);
+        };
+
     };
 
 })(jQuery, window, document);
